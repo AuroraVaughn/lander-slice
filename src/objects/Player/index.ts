d631a54d02cef460d7f7b700e9c7a66ca029962a
AuroraVaughn <vaughn.aurora@gmail.com>
2021-11-08 16:42:43 -0600

    test

--- src/objects/Player/index.ts
+++ src/objects/Player/index.ts
@@ -4,16 +4,11 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
   commands: IKeyListeners;
   targetDirection: number;
   ROTATION_SPEED = 1 * Math.PI;
-  get rotationSpeed (){
-    if(Math.abs(this.targetDirection)< .8){
-      return 3 * Math.PI;
-    }
-    return 1 * Math.PI;
-  }
+
   canStep = false;
-  STEP_THRUST = 0.0003;
-  RUN_MAINTAIN = 0.00004;
-  WALK_MAINTAIN = 0.00002;
+  STEP_THRUST = 1;
+  RUN_MAINTAIN = 0.0004;
+  WALK_MAINTAIN = 0.0002;
   constructor(
     public scene: Phaser.Scene,
     matterConfig: Phaser.Types.Physics.Matter.MatterBodyConfig
@@ -26,7 +21,8 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
       0,
       matterConfig
     );
-    this.body.mass = 0.04;
+    this.scale = .5
+    this.body.mass = 0.2;
     this.scene.add.existing(this);
     this.commands = getBasicKeyListeners();
     this.commands.step.on("down", this.pressStep);
@@ -35,8 +31,16 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
 
     this.scene.input.on("pointermove", this.rotate);
     this.scene.input.mouse.onMouseDown = this.attack;
+ 
   }
-
+  get rotationSpeed (){
+    if(Math.abs(this.targetDirection)< .8){
+      return 1.8 * Math.PI;
+    }else if(Math.abs(this.targetDirection) > 1.9){
+      return 1.8 * Math.PI;
+    }
+    return 1.5 * Math.PI;
+  }
   rotate = (event: MouseEvent) => {
     this.targetDirection = Phaser.Math.Angle.BetweenPoints(this, event);
     console.log("targetDirection->", this.targetDirection)
@@ -47,8 +51,6 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
   };
   pressStep = (event: KeyboardEvent) => {
     this.thrust(0.0003);
-
-    console.log(this);
   };
   holdStep = (event: KeyboardEvent) => {
     console.log(event, this.scene);
@@ -58,9 +60,9 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
   };
   create() {}
   update() {
-    if(this.commands.step.getDuration() > 500){
-      this.activelyMoving()
-    }
+
+    
+    
     
     if (!Phaser.Math.Fuzzy.Equal(this.rotation, this.targetDirection)) {
       
--- src/scenes/Demo/index.ts
+++ src/scenes/Demo/index.ts
@@ -11,16 +11,25 @@ export default class Demo extends Phaser.Scene {
 
   preload() {
     this.load.image("player", "assets/player.png");
+    this.load.image("grass", "assets/grass01.png");
+
   }
 
   create() {
+    this.add.tileSprite(0,0,64,64, 'grass')
+    this.cameras.main.setViewport(0, 0, 150, 150); 
     this.inputKeys = createBasicKeyListeners(this)
-    this.player = new Player(this, { position: { x: 32, y: 32 } });
- 
-   
+    this.player = new Player(this, { position: { x: 200, y: 200 } });
+    this.cameras.main.setBounds(0, 0, 1000, 1000);
+    this.cameras.main.startFollow(this.player,false,.2,.2)
+    
   }
   update(){
   this.player.update()
+
+  }
+
+  render(){
 
   }
 }
